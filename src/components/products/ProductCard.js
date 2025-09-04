03b4b745b58dee4d585af3ef058e1287aff234ed
Onwuli <onwuliDavid8@gmail.com>
2025-09-04 21:52:27 +0100

    added aos animation

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import Link from "next/link";
 import Image from 'next/image';
+import styled from 'styled-components';
 
 function ProductCard({product}) {
   return (
     <div>
         <Link href={`/products/${product.id}`} >
-            <div  style={{
-            border: "1px solid #eee",
-            borderRadius: "8px",
-            padding: "1rem",
-            background: "#fff",
-            boxShadow: "0 2px 8px rgba(0,0,0,0.05)",
-            // maxWidth:"250px",
-            }}>
+            <ProductContainer  >
                 <Image
                     src={product.image}
                     alt={product.name}
-                    width={100}
-                    height={180}
-                    style={{ width: "100%", height: "180px", objectFit: "cover", borderRadius: "6px" }}
+                    width={200}
+                    height={280}
+                    style={{ width: "100%", height: "280px", objectFit: "", borderRadius: "6px" }}
                 />
                 <h2 style={{ fontSize: "1.2rem", margin: "1rem 0 0.5rem" }}>{product.name}</h2>
                 <p style={{ color: "#888", marginBottom: "0.5rem" }}>{product.description}</p>
@@ -27,7 +21,7 @@ function ProductCard({product}) {
                 <button
                 style={{
                     margin: "1rem 0 0 1rem",
-                    padding: "0.5rem 1rem",
+                    padding: "0.5rem 0.5rem",
                     background: "#e91e63",
                     color: "#fff",
                     border: "none",
@@ -37,10 +31,37 @@ function ProductCard({product}) {
                 >
                 Add to Cart
                 </button>
-            </div>
+                <button
+                style={{
+                    margin: "1rem 0 0 1rem",
+                    padding: "0.5rem 1rem",
+                    background: "#8a1ee9fc",
+                    color: "#fff",
+                    border: "none",
+                    borderRadius: "4px",
+                    cursor: "pointer"
+                }}
+                >
+                Buy Now
+                </button>
+            </ProductContainer>
             </Link>
     </div>
   )
 }
+const ProductContainer = styled.div`
+    border: 1px solid #eee;
+    borderRadius: 8px;
+    padding: 1rem;
+    background: #fff;
+    boxShadow: 0 2px 8px rgba(0,0,0,0.05);
+
+    &:hover {
+        img{
+         object-fit:cover;
+    
+        }
+    }
+`
 
 export default ProductCard
