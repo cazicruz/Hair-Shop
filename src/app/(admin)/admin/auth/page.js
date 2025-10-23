2922498a7c70e5a7a095a97392d4350d94aa7e5e
Onwuli <onwuliDavid8@gmail.com>
2025-10-23 12:04:52 +0100

    hot fix added Suspense boundry to login foarm

--- src/app/(admin)/admin/auth/page.js
+++ src/app/(admin)/admin/auth/page.js
@@ -1,4 +1,6 @@
-import React from 'react';
+export const dynamic = 'force-dynamic';
+
+import React,{Suspense} from 'react';
 import LoginForm from '@/components/modals/LoginForm';
 import { HeaderText } from '@/components/home/Latest';
 
@@ -7,8 +9,10 @@ export default function UsersPage() {
     <div>
       <h2>B-classy</h2>
       <HeaderText>Admin Login</HeaderText>
+          <Suspense fallback={<div>Loading...</div>}>
       <LoginForm openModal={true} 
       />
+      </Suspense>
     </div>
   );
 }
