55f9e364740fcca55ebf1f9c4632c29a1b120600
Onwuli <onwuliDavid8@gmail.com>
2025-10-23 12:18:35 +0100

    hot fix added Suspense boundry to login foarm

--- src/app/(admin)/admin/auth/page.js
+++ src/app/(admin)/admin/auth/page.js
@@ -1,11 +1,11 @@
 export const dynamic = 'force-dynamic';
 
 import React from 'react';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import { HeaderText } from '@/components/home/Latest';
 
 // Load LoginForm only on the client to avoid SSR/prerender errors from client-only libs (antd, react-toastify, etc.)
-const LoginForm = dynamic(() => import('@/components/modals/LoginForm'), { ssr: false });
+// const LoginForm = dynamic(() => import('@/components/modals/LoginForm'), { ssr: false });
 
 export default function UsersPage() {
   return (
